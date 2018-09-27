44f8e09b8e8e881dde32ee2c8c81bca47550d9bc
considerate-template-4770 <rafaelb24@yahoo.com>
2018-09-27 21:58:05 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,6 @@
 # Write your #display_rainbow method here
 def display_rainbow(["red", "orange", "yellow", "green", "blue", "indigo", "violet"])
-  puts "R #{display_rainbow[0]}"
+  puts "R: #{display_rainbow[0]}"
 end
 
 display_rainbow
